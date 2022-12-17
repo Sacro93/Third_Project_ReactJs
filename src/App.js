b91aab770aa0126e87a5840df062be772b131296
sacroisky francisco <sacroisky.f@gmail.com>
2022-12-16 21:25:14 -0300

    comenzando practicas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 //esto es lo que se renderiza en "index.js" "<App/>"
+
+//no se tiene todo en un mismo archivo , todo por separado y luego se "importa" , webpack lo une
 function App() {
+
+  const title= " Bienvenido! Empecemos con  React"
+  function hacerAlgo(){
+    alert ("hola")
+  }
+  const miEstilo={
+    backgroundColor: "pink",
+    margin: "30px",
+    border:"outset"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,19 +21,26 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bienvenido! Empecemos con  React
+         {title}
+         
          
 
         </a>
       </header>
+      <div style={miEstilo}>
+              <button onClick={hacerAlgo}>Click me</button>
+
+         </div>
     </div>
   );
 }
+
 
 export default App;
