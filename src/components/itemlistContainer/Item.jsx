3ed8bd0c69932011f1f43deee38fc1c7af4efcc1
sacroisky francisco <sacroisky.f@gmail.com>
2023-01-05 16:39:30 -0300

    editando card + fakeapi

--- src/components/itemlistContainer/Item.jsx
+++ src/components/itemlistContainer/Item.jsx
@@ -2,19 +2,23 @@ import Button from "../button/Button";
 import ItemCount from "../itemCount/ItemCount";
 
 function Item(props) {
-  const { title, price, img, detail } = props.item;
+  const { title, price, image, description } = props.item;
   
   return (
-    <div className="flexCard">
-      <img height="220px" src={img} alt={title} />
-      <h3>{title}</h3>
-      <small>{detail}</small>
-      <h3>$ {price}</h3>
+    <div className="flexCard ">
+      <div  className="card" ></div>
+      <div className="card-body " >
+      <h5 className="card-title mb-5">{title}</h5>
+      <img className="image" src={image} alt={title} />
+     
+      {/* <p className="mt-3">{description}</p> */}
+      <h4 className="mt-3">$ {price}</h4>
       <br />
-      <Button padding="20px" >
+      <Button >
         Add to cart
       </Button>
-      <ItemCount stock={5} />
+      <ItemCount  stock={5} />
+      </div>
     </div>
   );
 }
